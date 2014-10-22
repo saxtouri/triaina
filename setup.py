2f2d321e5e1e90e317402ec45e53457b9d43e58b
Stavros Sachtouris <saxtouri@gmail.com>
2014-10-23 02:53:16 +0300

    Create initial files and package root

---
+++ setup.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2014, Stavros Sachtouris
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from setuptools import setup
+import triaina
+
+setup(
+    name='triaina',
+    version=triaina.__version__,
+    description=('A kamaki clone done differently'),
+    long_description=open('README.md').read(),
+    url='http://github.com/saxtouri/triaina',
+    download_url='https://github.com/saxtouri/triaina/archive/master.zip',
+    license='BSD',
+    author='Stavros Sachtouris',
+    author_email='saxtouri@gmail.com',
+    maintainer='Stavros Sachtouris',
+    maintainer_email='saxtouri@gmail.com',
+    packages=['triaina', ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Topic :: System :: Shells',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+        ],
+    include_package_data=True,
+    entry_points={
+    },
+    install_requires=[]
+)
